894dfd2e1749bb49b2da610f07e420d63602dc98
Arjun <144340917+arjuunns@users.noreply.github.com>
2025-02-03 00:37:03 +0530

    Update Heading.jsx

--- TweetEnhancer/src/components/Heading.jsx
+++ TweetEnhancer/src/components/Heading.jsx
@@ -1,14 +1,14 @@
 const AITextBanner = () => {
   return (
     <div className="w-full py-24 px-4 flex flex-col items-center justify-center text-center">
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 dark:text-white">
         Transform Your Tweets with{" "}
         <span className="bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
           BetterTweets
         </span>
       </h1>
 
-      <p className="text-lg md:text-xl text-gray-500 max-w-2xl translate-y-4">
+      <p className="text-lg md:text-xl text-gray-500 max-w-2xl translate-y-4 dark:text-zinc-100">
         Instantly craft better tweets with our AI-powered editor! Adjust tone,
         style, effortlessly while optimizing hashtags for maximum
         engagement.
